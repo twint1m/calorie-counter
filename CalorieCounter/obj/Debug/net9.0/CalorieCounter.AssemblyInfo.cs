c2ee685027987dea70dc4fa0a24583524bb7e8bf
twint1m <twint1m.contact@gmail.com>
2025-06-30 22:07:31 +0300

    final

--- CalorieCounter/obj/Debug/net9.0/CalorieCounter.AssemblyInfo.cs
+++ CalorieCounter/obj/Debug/net9.0/CalorieCounter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalorieCounter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d4c708873f30a2271256539a3d51b76cb8feae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695e2ff328b4e6fa1953e44d275ba174832fd47e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalorieCounter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalorieCounter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
